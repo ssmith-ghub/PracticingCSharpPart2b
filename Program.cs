e079d4afd96d91b935a8e111d79735d5387da74e
Stephen Smith <ssmith_138823@outlook.com>
2024-04-02 20:42:55 -0400

    pushing to GitHub

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.WriteLine("world!\tMy name is Stephenn\b!\nI'm learning C#!");
+
+            //comment
+            /* 
+             * 
+             * 
+             */
+            Console.ReadKey();
+        }
+    }
+}
